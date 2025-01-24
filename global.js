2af0b59bb93efa14554c70b90e9cd650cc325eca
RitvikMohindru <165727601+RitvikMohindru@users.noreply.github.com>
2025-01-24 14:10:20 -0800

    final js implementation

--- global.js
+++ global.js
@@ -37,8 +37,34 @@ for (let p of pages) {
 
     if (a.host === location.host && a.pathname === location.pathname) {
         a.classList.add('current');
-      }
+    }
     if (a.host !== location.host) {
         a.target = "_blank";
-      }
-  }
+    }
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',`
+    <label class="color-scheme">
+        Theme:
+        <select>
+            <option value="light dark">automatic</option>
+            <option value="light">light</option>
+            <option value="dark">dark</option>
+        </select>
+    </label>
+    `
+);
+
+const select = document.querySelector('.color-scheme select');
+
+select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+    localStorage.colorScheme = event.target.value;
+});
+
+if ("colorScheme" in localStorage) {
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+    select.value = localStorage.colorScheme;
+}
