e2606c9f5b253d47d05a553921ae928a7a728935
RitvikMohindru <165727601+RitvikMohindru@users.noreply.github.com>
2025-02-01 20:34:09 -0800

    projects

--- projects/projects.js
+++ projects/projects.js
@@ -4,3 +4,9 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
+
+document.addEventListener("DOMContentLoaded", function () {
+    const projectsTitle = document.querySelector(".projects-title");
+    const projectCount = document.querySelectorAll(".project").length;
+    projectsTitle.textContent = `${projectCount} projects`;
+});
